8b11ca79718c290deedf7f7ae426496d9906a442
Juan Pablo ANGARITA <juanpabloangarita@gmail.com>
2017-08-04 16:13:09 +0200

    Make a basic User model(including secure passwords)

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,29 +2,32 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
+def setup
+  @base = "Ruby on Rails Tutorial Sample App"
+end
 
 
   test "should get home" do
     get '/'
     assert_response :success
-    assert_select "title", "Ruby on Rails Tutorial Sample App"
+    assert_select "title", "#{@base}"
   end
 
   test "should get help" do
     get help_path
     assert_response :success
-    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Help | #{@base}"
   end
 
   test "should get about" do
     get about_path
     assert_response :success
-    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "About | #{@base}"
   end
 
   test "should get contact" do
     get contact_path
     assert_response :success
-    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Contact | #{@base}"
   end
 end
